4e319c08f9f1b37f3899dd4b478b2af67a8880e7
Gabriele Sarti <gabry01sa@gmail.com>
2021-05-10 22:31:40 +0200

    Added folders

---
+++ solver/solver.go
@@ -0,0 +1,16 @@
+package solver
+
+func solver(){
+	//Breadth-first search algorithm
+	//getting nodes:
+	//	-start from the start
+	//	-look down/sx/up/dx
+	//	-boolean array if visited
+	//	-for rows:
+	//		-first node at the start
+	//		-if im in a path and i was in a wall is a node
+	//		-if i can go up or down is a node
+	//		-if im in a path and i will be in a wall is a node
+	//		-if i cant go left or right isnt a node, and so on...
+	//		-count node the min is the solver
+}
